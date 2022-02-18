a0a9818744cf58d77261433d47600e5c7eb7aec5
NangSengHarn <nangsengharn@gmail.com>
2022-02-19 00:55:37 +0630

    filter function

---
+++ filter.py
@@ -0,0 +1,14 @@
+from ast import comprehension
+
+
+nums=[1,2,3,4,5,6,7,8,9,10]
+
+def even(num) :
+    return (num%2)==0
+
+evenNums=list(filter(even,nums))
+print(evenNums)
+
+# comprehension way
+# evenNums=[num for num in nums if (num%2)==0]
+# print(evenNums);
