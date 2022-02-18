dc23987b0a1c19ad2dd46a5c462a272ad4463d07
NangSengHarn <nangsengharn@gmail.com>
2022-02-19 00:46:41 +0630

    map function

---
+++ map.py
@@ -0,0 +1,14 @@
+from ast import comprehension
+
+
+nums=[2,5,6,7,8,9,10]
+
+def mapfunction(num):
+    return num*2
+
+nums=list(map(mapfunction,nums))
+print(nums);
+
+# comprehension way
+# nums=[num*2 for num in nums]
+# print(nums);
