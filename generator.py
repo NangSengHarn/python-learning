a07d6b3bead554ea132d649b455be5c3d6b06856
NangSengHarn <nangsengharn@gmail.com>
2022-02-19 19:55:02 +0630

    mixing custom word in gibberish generator

--- generator.py
+++ generator.py
@@ -1,8 +1,19 @@
+words=['apple','orange','lemon','avocado','banana'];
+
+from random import randint;
+
+def randomSentenceGenerator(word):
+    randomIndex=randint(0,len(words)-1)
+    return f'{words[randomIndex]} {word}'
+
 with open('./text.txt') as file:
     paragraph=file.read();
+    wordLists=paragraph.split()
+    sentenceList=list(map(randomSentenceGenerator,wordLists))
 
     paraCount=int(input('paragraph count :'))
+
     for count in range(paraCount):
         with open('./generator.txt','a') as write_file:
-            write_file.write(paragraph+'\n\n');
+            write_file.write(''.join(sentenceList)+'\n\n');
         
